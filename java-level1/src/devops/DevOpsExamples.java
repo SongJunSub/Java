c7a729043d2bc6494992352c9b8e001858307968
JunSub_Dev <song911130@gmail.com>
2025-07-05 17:56:05 +0900

    feat: DevOps(Terraform AWS EC2 배포, Helm Chart, GitOps 파이프라인) 실전 예제 및 문서화 추가

--- java-level1/src/devops/DevOpsExamples.java
+++ java-level1/src/devops/DevOpsExamples.java
@@ -1,33 +1,58 @@
 /*
- * 실전 DevOps/Infra as Code
- * - Terraform, Ansible, Helm, GitOps, IaC로 인프라 자동화
+ * 실전 DevOps/Infra as Code (심화)
+ * - Terraform으로 AWS EC2 배포, Helm Chart 실전, GitOps 파이프라인
  */
 
-// 예시: Terraform, Ansible, Helm, GitOps (주석 참고)
+// Terraform으로 AWS EC2 배포 예제 (주석 참고)
 /*
-// Terraform 예제
+# main.tf
+provider "aws" {
+  region = "ap-northeast-2"
+}
 resource "aws_instance" "example" {
   ami           = "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro"
+  tags = { Name = "devops-ec2" }
 }
+*/
 
-// Ansible 예제
-- hosts: all
-  tasks:
-    - name: Install Java
-      apt:
-        name: openjdk-21-jdk
-        state: present
-
-// Helm Chart 예제
+// Helm Chart 실전 예제 (주석 참고)
+/*
+# Chart.yaml
 apiVersion: v2
-kind: Chart
+name: myapp
+version: 0.1.0
+
+# values.yaml
+replicaCount: 2
+image:
+  repository: myapp
+  tag: latest
+service:
+  type: ClusterIP
+  port: 80
+*/
+
+// GitOps 파이프라인 예제 (ArgoCD, 주석 참고)
+/*
+# argo-app.yaml
+apiVersion: argoproj.io/v1alpha1
+kind: Application
 metadata:
   name: myapp
+spec:
+  source:
+    repoURL: 'https://github.com/myorg/myapp.git'
+    path: charts/myapp
+    targetRevision: HEAD
+  destination:
+    server: 'https://kubernetes.default.svc'
+    namespace: default
+  project: default
 */
 
 public class DevOpsExamples {
     public static void main(String[] args) {
-        System.out.println("Terraform, Ansible, Helm, GitOps 등은 주석의 예시를 참고하세요.\n실행은 별도 환경에서 가능합니다.");
+        System.out.println("Terraform AWS EC2 배포, Helm Chart, GitOps 파이프라인 등은 주석의 예시를 참고하세요.\n실행은 별도 환경에서 가능합니다.");
     }
 } 
