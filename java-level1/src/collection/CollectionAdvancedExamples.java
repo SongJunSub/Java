b6d9d31556b37147c2f7285bcd71ea8758a76148
JunSub_Dev <song911130@gmail.com>
2025-07-19 21:10:59 +0900

    refactor: Refactor CollectionAdvancedExamples and add package declaration

--- java-level1/src/collection/CollectionAdvancedExamples.java
+++ java-level1/src/collection/CollectionAdvancedExamples.java
@@ -1,48 +1,76 @@
-/*
- * 컬렉션 프레임워크 심화
- * - List, Set, Map의 다양한 구현체 비교 및 활용
- * - 불변 컬렉션, 동기화 컬렉션, Concurrent 컬렉션
- */
+package collection;
+
 import java.util.*;
 import java.util.concurrent.*;
 
 public class CollectionAdvancedExamples {
-    public static void main(String[] args) {
-        // 1. 다양한 List 구현체
+
+    public void listImplementations() {
+        // ArrayList: 내부적으로 배열을 사용. 조회는 빠르나, 추가/삭제는 느릴 수 있음.
         List<String> arrayList = new ArrayList<>();
-        List<String> linkedList = new LinkedList<>();
-        arrayList.add("A"); arrayList.add("B");
-        linkedList.add("X"); linkedList.add("Y");
+        arrayList.add("A");
+        arrayList.add("B");
         System.out.println("ArrayList: " + arrayList);
+
+        // LinkedList: 내부적으로 양방향 연결 리스트를 사용. 추가/삭제는 빠르나, 조회는 느릴 수 있음.
+        List<String> linkedList = new LinkedList<>();
+        linkedList.add("X");
+        linkedList.add("Y");
         System.out.println("LinkedList: " + linkedList);
+    }
 
-        // 2. Set 구현체 (HashSet, TreeSet)
+    public void setImplementations() {
+        // HashSet: 순서를 보장하지 않음. 중복을 허용하지 않음.
         Set<String> hashSet = new HashSet<>(Arrays.asList("A", "B", "A"));
-        Set<String> treeSet = new TreeSet<>(Arrays.asList("B", "A", "C"));
         System.out.println("HashSet: " + hashSet);
+
+        // TreeSet: 정렬된 순서를 보장함. 중복을 허용하지 않음.
+        Set<String> treeSet = new TreeSet<>(Arrays.asList("B", "A", "C"));
         System.out.println("TreeSet(정렬): " + treeSet);
+    }
 
-        // 3. Map 구현체 (HashMap, TreeMap)
+    public void mapImplementations() {
+        // HashMap: 순서를 보장하지 않음.
         Map<String, Integer> hashMap = new HashMap<>();
-        Map<String, Integer> treeMap = new TreeMap<>();
-        hashMap.put("one", 1); hashMap.put("two", 2);
-        treeMap.put("one", 1); treeMap.put("two", 2);
+        hashMap.put("one", 1);
+        hashMap.put("two", 2);
         System.out.println("HashMap: " + hashMap);
+
+        // TreeMap: 키에 따라 정렬된 순서를 보장함.
+        Map<String, Integer> treeMap = new TreeMap<>();
+        treeMap.put("one", 1);
+        treeMap.put("two", 2);
         System.out.println("TreeMap(정렬): " + treeMap);
+    }
 
-        // 4. 불변 컬렉션
+    public void immutableCollections() {
+        // List.of()로 생성된 리스트는 불변.
         List<String> immutableList = List.of("A", "B");
-        // immutableList.add("C"); // UnsupportedOperationException
         System.out.println("불변 List: " + immutableList);
+        // immutableList.add("C"); // 실행 시 UnsupportedOperationException 발생
+    }
 
-        // 5. 동기화 컬렉션
+    public void synchronizedCollections() {
+        // Collections.synchronizedList()로 동기화된 리스트를 생성.
         List<String> syncList = Collections.synchronizedList(new ArrayList<>());
         syncList.add("A");
         System.out.println("동기화 List: " + syncList);
+    }
 
-        // 6. Concurrent 컬렉션
+    public void concurrentCollections() {
+        // ConcurrentHashMap: 동시성 환경에서 안전하게 사용 가능.
         ConcurrentMap<String, Integer> concurrentMap = new ConcurrentHashMap<>();
         concurrentMap.put("A", 1);
         System.out.println("ConcurrentMap: " + concurrentMap);
     }
-} 
+
+    public static void main(String[] args) {
+        CollectionAdvancedExamples examples = new CollectionAdvancedExamples();
+        examples.listImplementations();
+        examples.setImplementations();
+        examples.mapImplementations();
+        examples.immutableCollections();
+        examples.synchronizedCollections();
+        examples.concurrentCollections();
+    }
+}
